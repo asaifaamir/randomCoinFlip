b8cd6c19e10fbc6e21ad6b0bf7923692fcd7f677
Asaif Butt <asaifbutt@asaifs-mbp>
2016-10-01 22:13:58 -0400

    Create the project for flipping a coin randomly 200 times Use the random library to generate a sequence of 0's and 1's to imitate coin flipping.

--- randomCoinFlipping/main.cpp
+++ randomCoinFlipping/main.cpp
@@ -1,15 +1,41 @@
 //
 //  main.cpp
 //  randomCoinFlipping
+//  Uses random library from C++11 and generates a sequence of 0's and 1's that are interprated as Heads and Tails in coin flips. The project is created as a part of a simulation for Stochastic Models.
 //
 //  Created by Asaif Butt on 10/1/16.
 //  Copyright © 2016 Asaif Butt. All rights reserved.
 //
 
 #include <iostream>
+#include <ctime>
+#include <random>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    
+    int numberFlips;
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0,1);
+    
+    cout << "Please enter the number of flips you want to do: ";
+    cin >> numberFlips;
+
+    for(int i = 0; i < numberFlips; ++i)
+    {
+        int coinFlip = distribution(generator);
+      
+        
+        if (coinFlip == 0)
+        {
+            cout << "Heads" << ' ';
+        }else
+        {
+            cout << "Tails" << ' ';
+        }
+    }
+
+    
     return 0;
 }
