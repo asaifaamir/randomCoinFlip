a6efd2e41c53651f26f8d8fa4dcded51687c3bef
Asaif Butt <asaifbutt@asaifs-mbp>
2016-10-01 21:38:53 -0400

    Initial Commit

---
+++ randomCoinFlipping/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  randomCoinFlipping
+//
+//  Created by Asaif Butt on 10/1/16.
+//  Copyright © 2016 Asaif Butt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
